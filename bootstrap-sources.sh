859a9034203deb4de0b6e26010dd2f0052c004c1
Simon Cook <simon.cook@embecosm.com>
2021-04-07 13:32:33 +0100

    Switch to allfixes-rebase

--- bootstrap-sources.sh
+++ bootstrap-sources.sh
@@ -3,4 +3,4 @@ git clone https://sourceware.org/git/binutils-gdb.git binutils-gdb
 git clone https://github.com/llvm/llvm-project.git llvm-project
 git clone https://sourceware.org/git/newlib-cygwin.git newlib
 git clone https://github.com/embecosm/riscv-binutils-gdb.git -b spc-cgen-sim-rve binutils-gdb-sim
-git clone https://github.com/embecosm/gcc-for-llvm-testing.git -b allfixes-rebase-tree gcc-for-llvm-testing
+git clone https://github.com/embecosm/gcc-for-llvm-testing.git -b allfixes-rebase gcc-for-llvm-testing
