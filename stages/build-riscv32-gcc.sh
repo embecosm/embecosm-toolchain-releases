45392ef431283841c0d99acbcd7e10b5c622415a
Simon Cook <simon.cook@embecosm.com>
2023-01-12 13:15:25 +0000

    GDB: Update build of libgmp to use integrated sources
    GDB now builds any libgmp found in the 'gmp' folder within the
    binutils-gdb source, replacing support for our previous approach of
    building libgmp ahead of time and providing the install folder via
    --with-libgmp-prefix.
    
    This adds a derivitive of the prepare-libgmp.sh script to download
    libgmp into a given directory. The three build scripts have also
    been updated to match this approach. MPFR is also downloaded at the
    same time as both projects are required to build GDB.

--- stages/build-riscv32-gcc.sh
+++ stages/build-riscv32-gcc.sh
@@ -32,11 +32,9 @@ if [ "x${PARALLEL_JOBS}" == "x" ]; then
   PARALLEL_JOBS=$(nproc)
 fi
 
-# If a local GMP build is not available, download and build it
-source utils/prepare-libgmp.sh
-
 # Binutils-gdb - Do in one step if possible
 if [ -e "binutils-gdb" ]; then
+  source utils/download-libgmp.sh binutils-gdb
   mkdir -p ${BUILDPREFIX}/binutils-gdb
   cd ${BUILDPREFIX}/binutils-gdb
   CFLAGS="-g -O2 -Wno-error=implicit-function-declaration" \
@@ -45,13 +43,13 @@ if [ -e "binutils-gdb" ]; then
       --target=riscv32-unknown-elf    \
       --prefix=${INSTALLPREFIX}       \
       --with-expat                    \
-      --with-libgmp-prefix=${SRCPREFIX}/gmp-${LIBGMP_VERS}/inst \
       --disable-werror                \
       ${EXTRA_OPTS}                   \
       ${EXTRA_BINUTILS_OPTS}
   make -j${PARALLEL_JOBS}
   make install
 else
+  source utils/download-libgmp.sh gdb
   # Binutils
   mkdir -p ${BUILDPREFIX}/binutils
   cd ${BUILDPREFIX}/binutils
@@ -75,7 +73,6 @@ else
       --target=riscv32-unknown-elf    \
       --prefix=${INSTALLPREFIX}       \
       --with-expat                    \
-      --with-libgmp-prefix=${SRCPREFIX}/gmp-${LIBGMP_VERS}/inst \
       --disable-werror                \
       ${EXTRA_OPTS}                   \
       ${EXTRA_BINUTILS_OPTS}
