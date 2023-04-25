e62ae5c6ebff680e769f5f1bf7198a49701cf882
Arthur Cohen <arthur.cohen@embecosm.com>
2023-04-25 16:51:03 +0200

    gccrs: Add base build and test script

---
+++ stages/build-gccrs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -xe
+# Script for building a gccrs Toolchain from checked out sources
+
+# Copyright (C) 2023 Embecosm Limited
+
+# Contributor: Arthur Cohen <arthur.cohen@embecosm.com>
+
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Variables used in this script
+INSTALLPREFIX=${PWD}/install
+BUILDPREFIX=${PWD}/build
+SRCPREFIX=${PWD}
+
+# Print the GCC and G++ used in this build
+which gcc
+which g++
+
+# If a BUGURL and PKGVERS has been provided, set variables
+EXTRA_OPTS=""
+if [ "x${BUGURL}" != "x" ]; then
+  EXTRA_OPTS="${EXTRA_OPTS} --with-bugurl='${BUGURL}'"
+fi
+if [ "x${PKGVERS}" != "x" ]; then
+  EXTRA_OPTS="${EXTRA_OPTS} --with-pkgversion='${PKGVERS}'"
+fi
+
+# Allow environment to control parallelism
+if [ "x${PARALLEL_JOBS}" == "x" ]; then
+  PARALLEL_JOBS=$(nproc)
+fi
+
+# gccrs
+cd ${SRCPREFIX}/gccrs
+./contrib/download_prerequisites
+mkdir -p ${BUILDPREFIX}/gccrs
+cd ${BUILDPREFIX}/gccrs
+../../gccrs/configure                                   \
+    --enable-languages=rust                             \
+    --enable-multilib                                   \
+    ${EXTRA_OPTS}                                       \
+    ${EXTRA_GCC_OPTS}
+make -j${PARALLEL_JOBS}
+make install
